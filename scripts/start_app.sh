9fd0a55fc13190926d2610651182607bbe4e0bf9
aytuns <ayotunde.jerry@gmail.com>
2024-09-08 15:19:06 +0100

    changed ip

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/django-todo/todoApp/settings.py
+sed -i 's/\[]/\["3.91.76.203"]/' /home/ubuntu/django-todo/todoApp/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
